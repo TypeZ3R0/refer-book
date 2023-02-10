254f0b12e52352d8eeb5b24f5c9ec9ff22247d3e
Kuntal <sardarkuntal49@gmail.com>
2023-02-10 15:38:20 +0530

    Add register page and css

---
+++ routes/user_routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+import { userRegister } from "../controllers/user_controllers.js";
+
+const router = Router();
+
+router.get("/register", userRegister);
+
+export default router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 
 // Importing local modules
 import generalRoutes from "./routes/general_routes.js";
+import userRoutes from "./routes/user_routes.js";
 
 dotenv.config();
 
@@ -11,10 +12,11 @@ dotenv.config();
 const app = express();
 
 // Setting view engine (ejs)
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Routes middleware
 app.use("/", generalRoutes);
+app.use("/accounts", userRoutes);
 
 // Listening to PORT
 const PORT = process.env.PORT || 8000;
